8b508a07ce6eeeb2826088b8a447c16969706943
Ropi <rolaca11@sch.bme.hu>
2020-04-02 01:24:49 +0200

    comment

--- src/test/java/demo/DemoTest.java
+++ src/test/java/demo/DemoTest.java
@@ -10,7 +10,7 @@ class DemoTest {
     @Test
     void resourceTest() throws IOException {
         Thread.currentThread().getContextClassLoader().getResources("demo").asIterator().forEachRemaining(url -> {
-            System.out.println(url.getFile());
+            System.out.println(url.getFile()); // I'd like to see the target/classes/demo directory here at some point.
         });
     }
 }
